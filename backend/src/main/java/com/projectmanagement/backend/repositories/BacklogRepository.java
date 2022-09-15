0dd9e69ebce418ddb1df0392adb0d2002fbc68ab
nikonikpay <nikonikpay@gmail.com>
2022-09-15 13:01:01 +0200

    Project management

---
+++ backend/src/main/java/com/projectmanagement/backend/repositories/BacklogRepository.java
@@ -0,0 +1,12 @@
+package com.projectmanagement.backend.repositories;
+
+
+import com.projectmanagement.backend.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+    Backlog findByProjectIdentifier(String Identifier);
+}
